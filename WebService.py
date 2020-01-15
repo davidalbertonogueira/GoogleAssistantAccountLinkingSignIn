d4421ee0e495b7f4826253f4353f849725c04329
davidalbertonogueira <david_alberto_nogueira@hotmail.com>
2020-01-15 18:05:47 +0000

    Add exported Dialogflow project. Improved WebService.py

--- WebService.py
+++ WebService.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from flask import Flask
-from flask_assistant import Assistant, ask, profile, sign_in
-import json
+from flask import Flask, jsonify, abort, make_response, request, render_template, flash, redirect
+from flask_assistant import Assistant, ask
+from flask_assistant import profile, sign_in
+from flask_assistant import session_id, user, storage
+from flask_assistant import request as assistant_request, context_manager,  event, storage
 
+
+import json
+import uuid
 
 app = Flask(__name__)
 
@@ -19,18 +24,56 @@ assist = Assistant(app=app, route="/", project_id=private_data['project_id']) #P
 
 @assist.action("Default Welcome Intent")
 def welcome():
+    print("Default Welcome Intent")
+    print("Request: ", request)
+    print("assistant_request: ", assistant_request)
+
     if profile:
-        return ask(f"Welcome back {profile['name']}")
-    return ask("Hi! I'm your Auth test App. It's a pleasure. Remember, if you want to authenticate, please say 'I want to sign in\".")
+        print("Profile: ", profile)
+    if session_id:
+        print("session_id: ",session_id)
+    if user:
+        print("user: ",user)
+    if storage:
+        print("storage: ",storage)
+    
+    output_message = ""
+
+    if profile:
+        output_message+=f"Welcome back {profile['name']}. "
+    else:
+        output_message+="Hi! I'm your Authentication Tester app. It's a pleasure! Remember, if you want to authenticate, please say 'I want to sign in\". "
+    
+    if user:
+        if user.get('userStorage') is None or len(user['userStorage'])==0 :
+            output_message += "Looks like you're new here. "
+            user['userStorage'] = {}
+            uid = uuid.uuid4()
+            user['userStorage']['ID'] = str(uid)
+            output_message += "I've given you the following ID: " + user['userStorage']['ID'] + " . "
+        
+        elif user['userStorage'].get('ID') is not None:
+            last_seen = user['lastSeen']
+            output_message += ("Welcome back " + 
+                user['userStorage']['ID'] + 
+                " ! Haven't seen you since "+ last_seen + ".")
+
+    return ask(output_message)
 
 @assist.action("Start-Sign-In")
 def start_sign_in():
+    print("Start-Sign-In")
+    print("Request: ", request.data)
+    print("assistant_request: ", assistant_request)
     return sign_in("Sure! Let's do it! To learn more about you")
 
 # this intent must have the actions_intent_SIGN_IN event
 # and will be invoked once the user has 
 @assist.action("Complete-Sign-In")
 def complete_sign_in():
+    print("Complete-Sign-In")
+    print("Request: ", request)
+    print("assistant_request: ", assistant_request)
     if profile:
         return ask(f"Welcome aboard {profile['name']}, thanks for signing up!")
     else:
